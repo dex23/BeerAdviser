79d73d3d4a1aa1427ece61cba89e450f944b246a
vladaB91 <vladabecelic@hotmail.com>
2017-03-10 16:51:51 +0100

    Izmene u activity-u

--- app/src/main/java/com/vladabecelic/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/vladabecelic/beeradviser/FindBeerActivity.java
@@ -1,9 +1,15 @@
 package com.vladabecelic.beeradviser;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
+
+    public void onClickFindBeer(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
