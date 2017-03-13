74154d8b69f0d6b669904c0e18928082bd19a522
vladaB91 <vladabecelic@hotmail.com>
2017-03-13 18:50:22 +0100

    Odradjen spinner

--- app/src/main/java/com/vladabecelic/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/vladabecelic/beeradviser/FindBeerActivity.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
-    public void onClickFindBeer(View view){
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
     }
 }
